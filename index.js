15e29485b56f36f51d145efd97d4103a27f4877d
Tyler (xps) <tyler@xps.15>
2018-10-01 12:40:18 -0500

    :tada:

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const PromiseFtp = require('promise-ftp');
+const archiver = require('archiver');
+
+module.exports = async function upload(dir, filename) {
+
+  var ftp = new PromiseFtp();
+  ftp.connect({host: process.env.FTP_HOST, user: process.env.FTP_USER, password: process.env.FTP_PWD})
+  .then(() => {
+    return ftp.put(`./${dir}/${filename}`, filename);
+  }).then(() => {
+    return ftp.end();
+  });
+
+}
+
+module.exports = async function compressDir(sourceDir, outputDir, outputFilename) {
+  return new Promise((resolve,reject) => {
+    
+    // create a file to stream archive data to.
+    const zip = fs.createWriteStream(`./${outputDir}/${outputFilename}`);
+
+    const archive = archiver('zip', {
+      zlib: { level: 9 } // Sets the compression level.
+    });
+
+    // 'close' event is fired when a file descriptor is involved
+    zip.on('close', function() {
+      // console.log(archive.pointer() + ' total bytes compressed');
+      resolve('fail');
+    });
+
+    archive.on('error', function(err) {
+      reject(err);
+    });
+
+    // pipe archive data to the file
+    archive.pipe(zip);
+
+    // append files from a sub-directory, putting its contents at the root of archive
+    archive.directory(`./${sourceDir}/`, false);
+
+    // finalize the archive
+    archive.finalize();
+
+  });
+}
